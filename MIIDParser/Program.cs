f300555ea95d92e223dce8f42909d765209befc9
alin.paiu <palineduard@gmail.com>
2022-03-21 15:33:37 +0200

    Midi Parser Console Project

---
+++ MIIDParser/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MIDIParser
+{
+    class Program
+    {
+        private static void Main()
+        {
+            const string Path = "MidiFileTest.mid";
+
+            Console.WriteLine("Parsing: {0}\n", Path);
+
+            var midiFile = new MidiFile(Path);
+
+            Console.WriteLine("Format: {0}", midiFile.Format);
+            Console.WriteLine("TicksPerQuarterNote: {0}", midiFile.TicksPerQuarterNote);
+            Console.WriteLine("TracksCount: {0}", midiFile.TracksCount);
+
+            foreach (var track in midiFile.Tracks)
+            {
+                Console.WriteLine("\nTrack: {0}\n", track.Index);
+
+                foreach (var midiEvent in track.MidiEvents)
+                {
+                    const string Format = "{0} Channel {1} Time {2} Args {3} {4}";
+                    if (midiEvent.MidiEventType == MidiEventType.MetaEvent)
+                    {
+                        Console.WriteLine(
+                            Format,
+                            midiEvent.MetaEventType,
+                            "-",
+                            midiEvent.Time,
+                            midiEvent.Arg2,
+                            midiEvent.Arg3);
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            Format,
+                            midiEvent.MidiEventType,
+                            midiEvent.Channel,
+                            midiEvent.Time,
+                            midiEvent.Arg2,
+                            midiEvent.Arg3);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
