5e84fed82107101668bbb8f2790f6fa431e1c1ad
CristiGuliciuc99 <92685446+CristiGuliciuc99@users.noreply.github.com>
2022-05-16 19:36:18 +0300

    [PTE-24] Create UI
    * PTE_24_Create_UI
    
    * PTE_24_Bug_Fix_Anchor
    
    * PTE_24_Rename_Methods

--- MIDIConverter/MainForm.cs
+++ MIDIConverter/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,30 @@ namespace MIDIConverter
         {
             InitializeComponent();
         }
-    }
+
+		private void MainFormLoad(object sender, EventArgs e)
+		{
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+		}
+
+		private void PanelViewPaint(object sender, PaintEventArgs e)
+		{
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.FromArgb(95, 155, 173), 1);
+
+            Rectangle area = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+            LinearGradientBrush lgd = new LinearGradientBrush(area, Color.FromArgb(95, 155, 173), 
+                Color.FromArgb(245, 250, 250), 
+                LinearGradientMode.Vertical);
+            graphics.FillRectangle(lgd, area);
+            graphics.DrawRectangle(pen, area);
+		}
+
+		private void ButtonExitClick(object sender, EventArgs e)
+		{
+            Application.Exit();
+		}
+	}
 }
